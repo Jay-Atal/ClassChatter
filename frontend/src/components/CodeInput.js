4f62ef773413087a2c37177b3e5ad4025c0fb476
Tapugy <jakeitan@gmail.com>
2024-11-09 19:39:30 -0500

    ek

--- frontend/src/components/CodeInput.js
+++ frontend/src/components/CodeInput.js
@@ -1,15 +1,16 @@
 import { useState } from "react"
 import CodeBox from "./CodeBox";
 import CodeButton from "./CodeButton";
+import HostRoomBtn from "./HostRoomBtn";
 
 export default function CodeInput({join, host}){
 
     const [inputValue,setInputValue] = useState('')
     return(
-        <HostRoomBtn/>
+        <><HostRoomBtn />
         <div className="centered-flex">
-            <CodeBox val = {inputValue} setVal = {setInputValue}/>
-            <CodeButton val= {inputValue} setVal = {setInputValue} join = {join}/>
-        </div>
+            <CodeBox val={inputValue} setVal={setInputValue} />
+            <CodeButton val={inputValue} setVal={setInputValue} join={join} />
+        </div></>
     );
 }
--- frontend/src/components/HostRoomBtn.js
+++ frontend/src/components/HostRoomBtn.js
@@ -1,4 +1,8 @@
 export default function HostRoomBtn(){
+
+    const handleClick = ()=>{
+        
+    }
     return(
         <button onClick={handleClick} className= 'large-purple-button'>
             JOIN <i className="fa-solid fa-arrow-right"></i>
